32cdffcfcc49d460ca1035df163b2724b2bda271
castironclay <clay@Clays-MacBook-Pro.local>
2024-12-04 15:02:13 -0500

    Part 1 done

---
+++ day3/part1.py
@@ -0,0 +1,19 @@
+import re
+
+total = 0
+all_matches = 0
+re.findall
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+
+
+for line in lines:
+    matches = re.findall(r"mul\(\d+,\d+\)", line)
+    all_matches += len(matches)
+    for match in matches:
+        print(match)
+        numbers = re.findall(r"\d+", match)
+        value = int(numbers[0]) * int(numbers[1])
+        total += value
+print(f"Total Matches: {all_matches}")
+print(f" Total: {total}")
