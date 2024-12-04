5afeac3a6c05ccb3fa08480bbeecd035e2431140
castironclay <clay@Clays-MacBook-Pro.local>
2024-12-04 15:04:16 -0500

    Files

--- day3/part1.py
+++ day3/part1.py
@@ -2,10 +2,8 @@ import re
 
 total = 0
 all_matches = 0
-re.findall
 with open("input.txt", "r") as file:
     lines = file.readlines()
-
 
 for line in lines:
     matches = re.findall(r"mul\(\d+,\d+\)", line)
@@ -15,5 +13,6 @@ for line in lines:
         numbers = re.findall(r"\d+", match)
         value = int(numbers[0]) * int(numbers[1])
         total += value
+
 print(f"Total Matches: {all_matches}")
-print(f" Total: {total}")
+print(f"Total: {total}")
